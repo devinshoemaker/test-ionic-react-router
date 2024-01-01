f4cdcc9fcf08e24764bfab6e7774a34bf4e5e9d1
Devin Shoemaker <devinshoe@gmail.com>
2024-01-01 15:17:40 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test-ionic-react-router',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
