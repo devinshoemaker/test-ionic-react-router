f7915ffd0ae75e9c9a79a6fe30116bad1f90dc5d
Devin Shoemaker <devinshoe@gmail.com>
2024-01-02 10:39:19 -0600

    add e2e test

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,19 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useParams } from "react-router";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Page.css";
+import { Link } from "react-router-dom";
 
 const Page: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
+  const { name } = useParams<{ name: string }>();
 
   return (
     <IonPage>
@@ -25,6 +33,7 @@ const Page: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name={name} />
+        <Link to="/folder/Inbox/next">Go to next</Link>
       </IonContent>
     </IonPage>
   );
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,13 +4,18 @@ test.describe("Routes", () => {
   test("init", async ({ page }) => {
     await page.goto("/");
     await expect(page.getByRole("strong")).toHaveText("Inbox");
-    
+
     await page.reload();
     await expect(page.getByRole("strong")).toHaveText("Inbox");
-    
-    await page.locator('#inbox-list').getByText('Outbox').click();
+
+    await page.locator("#inbox-list").getByText("Outbox").click();
     await expect(page.getByRole("strong")).toHaveText("Outbox");
     await page.reload();
     await expect(page.getByRole("strong")).toHaveText("Outbox");
+
+    await page.getByRole("link", { name: "Go to next" }).click();
+    // await expect(page.getByRole("strong")).toHaveText("next");
+    await page.reload();
+    await expect(page.getByRole("strong")).toHaveText("next");
   });
 });
